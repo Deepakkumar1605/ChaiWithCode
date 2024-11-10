0735d9e79c0810962f6686b114dec44975c68dae
Your Name <your-email@example.com>
2024-11-10 20:37:19 +0530

    My code

---
+++ ChaiWithCode/01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 2545455;
+let accountEmail = "dutta.12@gmail.com";
+var accountPassword = "2455@ssd";
+accountCity = "jajpur";
+let accountState;
+
+// accountId = 5665656
+
+console.log(accountId);
+
+
+// To Console in Table Format then 
+//console.table([name,name])
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+  
+
+// Always Prefer not to use Var 
+// Beacause of issue in block scope and functional scope
+
