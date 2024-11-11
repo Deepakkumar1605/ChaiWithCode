282384506feba2b2416ee158a402af67a3681d14
DeepakKumarDutta <bapundutta879@gmail.com>
2024-11-11 21:19:06 +0530

    05

---
+++ ChaiWithCode/01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+// numbers 2,3
+//bigint
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//undefined  => value not assigned
+// symbol =>unique
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
