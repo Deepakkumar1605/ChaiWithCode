73d3ccd477b86f4dc3894afebd6ea7eaf6c4390a
DeepakKumarDutta <bapundutta879@gmail.com>
2024-11-12 23:07:20 +0530

    Conversion of Things

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,30 @@
+let Score = "356adasd"
+
+console.log(typeof Score);
+console.log(typeof(Score));
+
+let valueInNumber = Number(Score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "655alkl" =>NaN
+// true => 1; false =>0
+
+
+let isLoggedIn = 0
+
+let ValueOfLoggedIn = Boolean(isLoggedIn)
+console.log(ValueOfLoggedIn);
+
+// 1 => true; 0 => false
+// "hitesh" => true ; "" =>false
+
+let someNumber = 565
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
